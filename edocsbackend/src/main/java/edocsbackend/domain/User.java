6bcaf0b99f245407e6e50b23fee1e636709b50a4
Miguel Perello garcia <canyavall@gmail.com>
2017-07-16 12:38:28 +0200

    Auto stash before merge of "master" and "origin/master"

--- edocsbackend/src/main/java/edocsbackend/domain/User.java
+++ edocsbackend/src/main/java/edocsbackend/domain/User.java
@@ -13,7 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.AllArgsConstructor;
